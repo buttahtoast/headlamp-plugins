5fcdfea90bd2170aa06f3fa27852943bd93f6cde
Kevin Klopfenstein <kevin.klopfenstein@mobi.ch>
2025-11-19 09:04:46 +0100

    feat(capsule): add first capsule version

--- capsule/src/components/tenantProject.tsx
+++ capsule/src/components/tenantProject.tsx
@@ -6,14 +6,8 @@ export interface TenantProps {
   name?: string;
 }
 
-
 export function TenantDetail(props: TenantProps) {
   const params = useParams<{ name: string }>();
-  const { name = params.name} = props;
-  return (
-      <Resource.DetailsGrid
-        name={name}
-        resourceType={Tenants}
-      />
-  );
+  const { name = params.name } = props;
+  return <Resource.DetailsGrid name={name} resourceType={Tenants} />;
 }
